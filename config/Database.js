ba292ffbb8759030b3f56cc4a1d8ee075462da88
adeiskandarzulkarnaen <adeiskandarzulkarnaen@gmail.com>
2022-12-04 04:55:42 +0700

    add timezone

--- config/Database.js
+++ config/Database.js
@@ -3,6 +3,10 @@ import { Sequelize } from 'sequelize';
 const db = new Sequelize('his_db', 'root', '', {
   host: 'localhost',
   dialect: 'mysql',
+  dialectOptions: {
+    useUTC: false, // timezone for reading from database
+  },
+  timezone: '+07:00', // timezone for writing to database
 });
 
 export default db;
