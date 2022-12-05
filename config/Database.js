719fb26d9b642f4a4a722a6dd6e0edd79a1e3376
adeiskandarzulkarnaen <adeiskandarzulkarnaen@gmail.com>
2022-12-05 17:28:50 +0700

    use the environment variable in the Database.js

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,10 @@
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
 
-const db = new Sequelize('his_db', 'root', '', {
-  host: 'localhost',
+dotenv.config();
+
+const db = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, {
+  host: process.env.DB_HOSTNAME,
   dialect: 'mysql',
   dialectOptions: {
     useUTC: false, // timezone for reading from database
